1e85b4d4a84d6e0718122491936a47f3e0eea8ee
nhan <natshi@umich.edu>
2024-03-16 22:40:42 -0400

    Filtration: none discarded

--- data_analysis/cleaning.R
+++ data_analysis/cleaning.R
@@ -3,25 +3,43 @@ library("tidyverse")
 data <- read.csv("data_raw/data.csv")
 fillers <- read.csv("data_raw/fillers_grade.csv")
 
-data %>% select("Label", "Field name", "Field value", "PennElementType", "PennElementName", "Parameter", "Value", "partid", "sentence", "construct", "classification", "IO", "counter", "group", "item")
+# Prep experiment data:
+# Don't need quotes on column names
+data <- select(data, "Label", "Field name", "Field value", "PennElementType", "PennElementName", "Parameter", "Value", "partid", "sentence", "construct", "classification", "IO", "counter", "group", "item")
 
-# Loop through participant ids to calculate?
-for(id in unique(unlist(data[c("partid")]))) {
-  # Select all data where column = id
-  # Select all data where Parameter = Choice and PennElementName = test
-  curr_data <- subset(data, partid==id & Parameter=="Choice" & PennElementName=="test")
+# Filter data to only include experiment data (trials + fillers)
+data2 <- data |>
+  filter(PennElementName == "test" & Parameter == "Choice")
+
+# Calculate z-scores for each participant
+data2 <- data2 |> 
+  mutate(Value=as.numeric(Value), partid=as.factor(partid), item=as.factor(item)) |>
+  group_by(partid) |>
+  mutate(zscores=scale(Value))
+
+# Summary of means of z-scores and ratings by sentence construction (DO/PO) and classification (alternating/ non-alternating)
+data2 |>
+  filter(Label=="experimental_trial") |>
+  group_by(construct, classification) |> 
+  summarize(mean_z=mean(zscores, na.rm = TRUE), mean_rating=mean(Value, na.rm = TRUE))
   
-  # Select Choice values and calculate stdev and mean
-  # Assuming scores <- data[c(whatever meets criteria?)] 
-  # zscores <- (scores - mean(scores)) / sd(scores)
-  scores <- as.vector(curr_data$Value)
-  zscores <- (scores - mean(scores)) / sd(scores)
-  
-  # Loop through fillers where classification = high
-  # Compare zscore and increment count of unexpected
-  
-  # Loop through fillers where classification = low
-  
-  # If bad, then???
-  
-}
+# Process experiment data with filler criteria:  
+fillers <- fillers |>
+  mutate(item=as.factor(item))
+# Data for specified fillers
+filler_thres_dat <- data2 |>
+  semi_join(fillers, join_by(item))
+
+# group_by(partid, classification)
+# count: for high: sum(Value > 0) for low: sum(Value < 0)
+# mutate new column for correctness
+filler_thres_dat <- filler_thres_dat |> 
+  group_by(partid, classification) |>
+  mutate(correct=ifelse(classification=="low", ifelse(zscores < 0, 1, 0), ifelse(zscores > 0, 1, 0)))
+# Create list of bad partids
+temp <- filler_thres_dat |>
+  group_by(partid, classification) |>
+  filter((sum(correct, na.rm = TRUE) < 5 & classification == "high") & sum(correct, na.rm = TRUE) < 7 & classification == "low")
+
+# use anti_join to throw out partids that match bad ones
+# Unnecessary: none met the criteria for discarding
