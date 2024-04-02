d6873e11485e4821971968433dc9d064c4e47734
nhan <natshi@umich.edu>
2024-04-02 11:09:47 -0400

    Temp commit, I'm dying

--- data_analysis/analysis/cleaning.R
+++ data_analysis/analysis/cleaning.R
@@ -1,5 +1,9 @@
 library("tidyverse")
 
+
+# Experiment data preprocessing -------------------------------------------
+
+
 # Clean experiment data
 data <- read.csv("data_raw/data.csv")
 fillers <- read.csv("data_raw/fillers_grade.csv")
@@ -51,6 +55,8 @@ experiment_data <- data2 |>
 
 write.csv(experiment_data, "analysis/cleaned_experiment_data.csv", row.names=FALSE)
 
+
+# Initial model output ----------------------------------------------------
 
 # Prep model input
 stimuli <- read.csv('stimuli/cleaned_200_pairs.csv')
@@ -109,6 +115,8 @@ ggplot(data_sum, aes(x=human_diff, y=diff, color=classification)) +
 
 
 
+# 100M model data  --------------------------------------------------------
+
 # Process average model output (refresh dataframes because I closed the project, w)
 # Reenter experiment data and stimuli
 experiment_data <- read.csv("analysis/cleaned_experiment_data.csv")
@@ -122,34 +130,40 @@ temp <- stimuli |>
 experiment_data <- experiment_data |> 
   merge(temp[, c("sentence", "sent_pair_id")], by="sentence") |>
   relocate(sent_pair_id)
+# Add verbs to experiment data?
+verblist <- read.csv("data_raw/verblists.csv")
+experiment_data <- experiment_data |>
+  left_join(select(verblist, verb_id, verb), join_by(item == verb_id)) |>
+  rename(verb_id=item) |>
+  relocate(verb, .after=verb_id)
+
 
 # Read in model scores
 scores1 <- read.csv ("data_raw/wider_pairs_scored_100M_1.csv") |> 
   select(item, classification, frequency_rank, recipient_id, DOsentence, PDsentence, DO_score, PD_score, diff) |>
   mutate(sent_pair_id=row_number()) |>
   relocate(sent_pair_id) |>
-  rename(DO_score1=DO_score, PD_score1=PD_score, diff1=diff)
+  rename(DO_score1=DO_score, PD_score1=PD_score, diff1=diff, verb_id=item)
 scores2 <- read.csv ("data_raw/wider_pairs_scored_100M_2.csv") |> 
   select(item, classification, frequency_rank, recipient_id, DOsentence, PDsentence, DO_score, PD_score, diff) |>
   mutate(sent_pair_id=row_number()) |>
   relocate(sent_pair_id) |>
-  rename(DO_score2=DO_score, PD_score2=PD_score, diff2=diff)
+  rename(DO_score2=DO_score, PD_score2=PD_score, diff2=diff, verb_id=item)
 scores3 <- read.csv ("data_raw/wider_pairs_scored_100M_3.csv") |> 
   select(item, classification, frequency_rank, recipient_id, DOsentence, PDsentence, DO_score, PD_score, diff) |>
   mutate(sent_pair_id=row_number()) |>
   relocate(sent_pair_id) |>
-  rename(DO_score3=DO_score, PD_score3=PD_score, diff3=diff)
+  rename(DO_score3=DO_score, PD_score3=PD_score, diff3=diff, verb_id=item)
 scores4 <- read.csv ("data_raw/wider_pairs_scored_100M_4.csv") |> 
   select(item, classification, frequency_rank, recipient_id, DOsentence, PDsentence, DO_score, PD_score, diff) |>
   mutate(sent_pair_id=row_number()) |>
   relocate(sent_pair_id) |>
-  rename(DO_score4=DO_score, PD_score4=PD_score, diff4=diff)
-rename(DO_score1=DO_score, PD_score1=PD_score, diff1=diff)
+  rename(DO_score4=DO_score, PD_score4=PD_score, diff4=diff, verb_id=item)
 scores5 <- read.csv ("data_raw/wider_pairs_scored_100M_5.csv") |> 
   select(item, classification, frequency_rank, recipient_id, DOsentence, PDsentence, DO_score, PD_score, diff) |>
   mutate(sent_pair_id=row_number()) |>
   relocate(sent_pair_id) |>
-  rename(DO_score5=DO_score, PD_score5=PD_score, diff5=diff)
+  rename(DO_score5=DO_score, PD_score5=PD_score, diff5=diff, verb_id=item)
 
 scores1 |> 
   group_by(classification) |>
@@ -212,26 +226,88 @@ model_scores |>
     mean_DO=mean(DO_avg, na.rm=TRUE),
     mean_PD=mean(PD_avg, na.rm=TRUE)
   )
+model_scores <- model_scores |>
+  left_join(select(verblist, verb, verb_id), join_by(verb_id)) |>
+  relocate(verb, .after=verb_id)
+
+# Add pretrained GPT scores to model_scores
+gpt2_scores <- read.csv("data_raw/wider_pairs_scored_gpt2.csv") |>
+  select(item, classification, frequency_rank, recipient_id, DOsentence, PDsentence, DO_score, PD_score, diff) |>
+  mutate(sent_pair_id=row_number()) |>
+  relocate(sent_pair_id) |>
+  rename(DO_score_gpt2=DO_score, PD_score_gpt2=PD_score, diff_gpt2=diff, verb_id=item)
+gpt2_med_scores <- read.csv("data_raw/wider_pairs_scored_gpt2m.csv") |>
+  select(item, classification, frequency_rank, recipient_id, DOsentence, PDsentence, DO_score, PD_score, diff) |>
+  mutate(sent_pair_id=row_number()) |>
+  relocate(sent_pair_id) |>
+  rename(DO_score_gpt2_med=DO_score, PD_score_gpt2_med=PD_score, diff_gpt2_med=diff, verb_id=item)
+gpt2_large_scores <- read.csv("data_raw/wider_pairs_scored_gpt2l.csv") |>
+  select(item, classification, frequency_rank, recipient_id, DOsentence, PDsentence, DO_score, PD_score, diff) |>
+  mutate(sent_pair_id=row_number()) |>
+  relocate(sent_pair_id) |>
+  rename(DO_score_gpt2_large=DO_score, PD_score_gpt2_large=PD_score, diff_gpt2_large=diff, verb_id=item)
+
+gpt2_scores |>
+  group_by(classification) |>
+  summarize(
+    mean_diff=mean(diff_gpt2, na.rm=TRUE),
+    mean_DO=mean(DO_score_gpt2, na.rm=TRUE),
+    mean_PD=mean(PD_score_gpt2, na.rm=TRUE)
+  )
+
+model_scores <- model_scores |>
+  left_join(
+    select(gpt2_scores, DO_score_gpt2, PD_score_gpt2, diff_gpt2, sent_pair_id), 
+    join_by(sent_pair_id)) |>
+  left_join(
+    select(gpt2_med_scores, DO_score_gpt2_med, PD_score_gpt2_med, diff_gpt2_med, sent_pair_id), 
+    join_by(sent_pair_id)) |>
+  left_join(
+    select(gpt2_large_scores, DO_score_gpt2_large, PD_score_gpt2_large, diff_gpt2_large, sent_pair_id),
+    join_by(sent_pair_id)
+  )
+  
 write.csv(model_scores, "analysis/cleaned_model_data.csv")
 
 # Add model data to experiment data
 exp_mod_data <- experiment_data |>
   left_join(
-    select(model_scores, DO_avg, PD_avg, diff_avg, sent_pair_id), join_by(sent_pair_id)) |>
-  mutate(rel_score=case_when(construct=="PDsentence"~PD_avg, construct=="DOsentence"~DO_avg)) |>
-  select(-c(DO_avg, PD_avg)) |> 
+    select(
+      model_scores, 
+      DO_avg, 
+      PD_avg, 
+      diff_avg, 
+      DO_score_gpt2, 
+      PD_score_gpt2, 
+      diff_gpt2,
+      DO_score_gpt2_med,
+      PD_score_gpt2_med,
+      diff_gpt2_med,
+      DO_score_gpt2_large,
+      PD_score_gpt2_large,
+      diff_gpt2_large,
+      sent_pair_id), 
+    join_by(sent_pair_id)) |>
+  mutate(
+    score_100M=case_when(construct=="PDsentence"~PD_avg, construct=="DOsentence"~DO_avg),
+    score_gpt2=case_when(construct=="PDsentence"~PD_score_gpt2, construct=="DOsentence"~DO_score_gpt2),
+    score_gpt2med=case_when(construct=="PDsentence"~PD_score_gpt2_med, construct=="DOsentence"~DO_score_gpt2_med),
+    score_gpt2large=case_when(construct=="PDsentence"~PD_score_gpt2_large, construct=="DOsentence"~DO_score_gpt2_large)) |>
+  select(-c(DO_avg, PD_avg, DO_score_gpt2, PD_score_gpt2, DO_score_gpt2_med,
+            PD_score_gpt2_med, DO_score_gpt2_large, PD_score_gpt2_large)) |>
   left_join(select(stimuli, frequency_rank, sent_pair_id), join_by(sent_pair_id)) |>
   relocate(frequency_rank, .after=IO)
 
 # Get means and diffs
 part_mean_data <- exp_mod_data |>
-  group_by(sent_pair_id, construct, classification, IO, frequency_rank) |>
+  group_by(sent_pair_id, verb_id, verb, construct, classification, IO, 
+           frequency_rank, diff_avg, diff_gpt2, diff_gpt2_med, diff_gpt2_large) |>
   summarize(mean_z=mean(zscores), mean_scores=mean(Value)) |>
   pivot_wider(names_from=construct, values_from=c(mean_z, mean_scores)) |>
   mutate(
     human_zdiff=mean_z_PDsentence - mean_z_DOsentence, 
-    human_sdiff=mean_scores_PDsentence - mean_scores_DOsentence) |>
-  left_join(select(model_scores, diff_avg, sent_pair_id), join_by(sent_pair_id))
+    human_sdiff=mean_scores_PDsentence - mean_scores_DOsentence) # |>
+  # left_join(select(model_scores, diff_avg, sent_pair_id), join_by(sent_pair_id))
 write.csv(part_mean_data, "analysis/diff_means_data.csv")
 
 
@@ -251,7 +327,7 @@ part_mean_data |>
           mean_PD_z=mean(mean_z_PDsentence),
           mean_diff_z=mean(human_zdiff))
 
-ggplot(part_mean_data, aes(x=IO, y=human_sdiff, fill=classification)) + 
+ggplot(part_mean_data, aes(x=IO, y=human_zdiff, fill=classification)) + 
   geom_bar(position="dodge", stat="identity")
 ggplot(part_mean_data, aes(x=human_zdiff, y=diff_avg, color=IO)) +
   geom_point() +
@@ -264,3 +340,20 @@ ggplot(part_mean_data, aes(x=frequency_rank, y=diff_avg, color=classification)) 
   geom_point() +
   geom_smooth(method=lm)
 # Could maybe fit a linear transformation from human diffs to model diffs for comparison purposes?
+
+# group by verb 
+# use facet_wrap(vars(variable-to-split-by))
+temp <- exp_mod_data |>
+  mutate(sent_pair_id=as_factor(sent_pair_id))
+library(lme4)
+library(lmerTest)
+
+stats1 <- lm(Value~classification*construct, data=temp)
+summary(stats1)
+
+stats2 <- lmer(Value~classification*construct + frequency_rank + (1+classification*construct|partid) + (1+construct|sent_pair_id), data=temp)
+summary(stats2)
+
+# saving: ggsave
+# assign plots to object, save
+# can set dimensions, etc., in ggsave
